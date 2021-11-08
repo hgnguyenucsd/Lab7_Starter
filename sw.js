cd29d2355ed436cc7635c99a2261036c2fca3391
hgnguyenucsd <hgnguyen@ucsd.edu>
2021-11-07 23:58:16 -0800

    Lab 7 Expose mainly completed, Explore being worked on.

--- sw.js
+++ sw.js
@@ -1,14 +1,25 @@
 // sw.js - This file needs to be in the root of the directory to work,
 //         so do not move it next to the other scripts
 
-const CACHE_NAME = 'lab-7-starter';
+//Note: Code for Service Worker was applied from the Google Tutorial.
+
+const CACHE_NAME = "lab-7-starter";
+
+var urlsToCache = ["/", "/styles/main.css", "/script/main.js"];
 
 // Once the service worker has been installed, feed it some initial URLs to cache
-self.addEventListener('install', function (event) {
+self.addEventListener("install", function (event) {
   /**
    * TODO - Part 2 Step 2
    * Create a function as outlined above
    */
+
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
 /**
@@ -16,17 +27,61 @@ self.addEventListener('install', function (event) {
  * in the same scope do not need to be reloaded before their fetches will
  * go through this service worker
  */
-self.addEventListener('activate', function (event) {
+self.addEventListener("activate", function (event) {
   /**
    * TODO - Part 2 Step 3
    * Create a function as outlined above, it should be one line
    */
+
+  event.waitUntil(clients.claim());
+
+  var cacheAllowlist = [];
+
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (cacheName) {
+          if (cacheAllowlist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
 
 // Intercept fetch requests and store them in the cache
-self.addEventListener('fetch', function (event) {
+self.addEventListener("fetch", function (event) {
   /**
    * TODO - Part 2 Step 4
    * Create a function as outlined above
    */
+
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      // Cache hit - return response
+      if (response) {
+        return response;
+      }
+
+      return fetch(event.request).then(function (response) {
+        // Check if we received a valid response
+        if (!response || response.status !== 200 || response.type !== "basic") {
+          return response;
+        }
+
+        // IMPORTANT: Clone the response. A response is a stream
+        // and because we want the browser to consume the response
+        // as well as the cache consuming the response, we need
+        // to clone it so we have two streams.
+        var responseToCache = response.clone();
+
+        caches.open(CACHE_NAME).then(function (cache) {
+          cache.put(event.request, responseToCache);
+        });
+
+        return response;
+      });
+    })
+  );
 });
